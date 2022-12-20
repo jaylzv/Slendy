5a4cc107b5e82dd2d36ca400729622dfded955e5
jaylzv <jaylzv7@gmail.com>
2022-12-20 18:12:33 +0100

    All four papers

--- 100-game/100-game.js
+++ 100-game/100-game.js
@@ -43,6 +43,9 @@ class App extends Application {
         this.slenderman = await this.loader.loadNode('Slenderman');
 
         this.paper1 = await this.loader.loadNode('Paper1');
+        this.paper2 = await this.loader.loadNode('Paper2');
+        this.paper3 = await this.loader.loadNode('Paper3');
+        this.paper4 = await this.loader.loadNode('Paper4');
     }
 
     //quad.rotateY
@@ -53,11 +56,12 @@ class App extends Application {
         this.controller.update(dt);
         this.physics.update(dt);
 
-        // Slenderman timed teleportation and rotation
-        tajm += 0.001;
         px = this.collider.translation[0];
         py = this.collider.translation[2];
-        // console.log(px + " " + py);
+
+        // Slenderman timed teleportation and rotation
+        tajm += 0.001;
+        
         if (tajm%3 >= 0 && tajm%3 <= 0.001){
             console.log("x: " + px + " y: " + py);
             // Teleportation
@@ -95,14 +99,33 @@ class App extends Application {
             // }
         }
 
-        // Finding the paper
+        // Finding the papers
         // When we find the paper, it goes below the map
         // Paper1
         if ((px >= -5.5 && px <= -4.5) && (py <= -28.5 && py >= -29.5) && (this.paper1.translation[1] == 1)){
-            console.log("Najde list!");
+            console.log("Found the first paper!");
             papersFound += 1;
             console.log("Papers found: " + papersFound);
             this.paper1.translation = [-5.5, -5, -28.5];
+        }
+        // Paper2
+        if ((px >= -29.6 && px <= -28.5) && (py <= 5.5 && py >= 4.5) && (this.paper2.translation[1] == 1)){
+            console.log("Found the second paper!");
+            papersFound += 1;
+            console.log("Papers found: " + papersFound);
+            this.paper2.translation = [-29, -5, 5];
+        }
+        if ((px <= 25 && px >= 24) && (py >= 18.6 && py <= 19) && (this.paper3.translation[1] == 1)){
+            console.log("Found the third paper!");
+            papersFound += 1;
+            console.log("Papers found: " + papersFound);
+            this.paper3.translation = [24.5, -5, 19];
+        }
+        if ((px >= 29.2 && px <= 29.8) && (py >= -18.5 && py <= -17.5) && (this.paper4.translation[1] == 1)){
+            console.log("Found the fourth paper!");
+            papersFound += 1;
+            console.log("Papers found: " + papersFound);
+            this.paper4.translation = [29.4, -5, -18];
         }
         
     }
