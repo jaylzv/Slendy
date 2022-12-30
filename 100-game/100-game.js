557061b6554a3675178c560e2b3e405212eb6dd2
jaylzv <jaylzv7@gmail.com>
2022-12-30 19:19:22 +0100

    All 4 animals moving

--- 100-game/100-game.js
+++ 100-game/100-game.js
@@ -6,9 +6,14 @@ import { Renderer } from './Renderer.js';
 import { Physics } from './Physics.js';
 import { FirstPersonController } from '../../common/engine/FirstPersonController.js';
 
+import { Node } from '../common/engine/Node.js';
+
+import { quat } from '../lib/gl-matrix-module.js';
+
 // Iskoristeno za teleportiranjeto na slendy sekoi nekolku sekundi
 var tajm = 0;
 var trFlag = 0; // Translation flag
+var rotacija;
 
 // Player x and y
 var px;
@@ -24,6 +29,28 @@ var slTime = 0;
 // 1 if caught
 var caught = 0;
 
+// ANIMALS
+// Wolf x and y
+var wolfWay = 0;
+var wx;
+var wy;
+var wz;
+// Fox x and y
+var foxWay = 0;
+var fx;
+var fy;
+var fz;
+// Horse x and y
+var horseWay = 0;
+var hx;
+var hy;
+var hz;
+// Fox x and y
+var catWay = 0;
+var cx;
+var cy;
+var cz;
+
 class App extends Application {
 
     async start() {
@@ -33,6 +60,12 @@ class App extends Application {
         this.scene = await this.loader.loadScene(this.loader.defaultScene);
         this.camera = await this.loader.loadNode('Camera');
         this.collider = await this.loader.loadNode('Collider');
+        this.light =  await this.loader.loadNode('Light');
+        
+        this.light.rotation = this.camera.rotation;    
+        this.light.intensity = 0.5;
+        this.light.attenuation = [0.001, 0, 0.3];
+        this.light.color = [255, 255, 255];
 
         if (!this.scene || !this.camera) {
             throw new Error('Scene or Camera not present in glTF');
@@ -54,21 +87,43 @@ class App extends Application {
         this.paper2 = await this.loader.loadNode('Paper2');
         this.paper3 = await this.loader.loadNode('Paper3');
         this.paper4 = await this.loader.loadNode('Paper4');
+
+        this.wolf = await this.loader.loadNode('Wolf');
+        this.fox = await this.loader.loadNode('Fox');
+        this.horse = await this.loader.loadNode('Horse');
+        this.cat = await this.loader.loadNode('Cat');
     }
 
-    //quad.rotateY
+    // quat.rotateY
     // pogledaj dokumentacija za quat
     // this.slenderman.rotation = quat.create(quad.rotateY
 
     update(time, dt){
         this.controller.update(dt);
         this.physics.update(dt);
+        this.light.translation = this.collider.translation;
 
         px = this.collider.translation[0];
         py = this.collider.translation[2];
 
         sx = this.slenderman.translation[0];
         sy = this.slenderman.translation[2];
+
+        wx = this.wolf.translation[0];
+        wy = this.wolf.translation[2];
+        wz = this.wolf.translation[1];
+
+        fx = this.fox.translation[0];
+        fy = this.fox.translation[2];
+        fz = this.fox.translation[1];
+
+        hx = this.horse.translation[0];
+        hy = this.horse.translation[2];
+        hz = this.horse.translation[1];
+
+        cx = this.cat.translation[0];
+        cy = this.cat.translation[2];
+        cz = this.cat.translation[1];
 
         // Slenderman timed teleportation and rotation
         tajm += 0.001;
@@ -149,10 +204,60 @@ class App extends Application {
                 console.log("Slendy caught you!")
             }
         }
+
+        // Animals moving
+        // Wolf moving
+        if (wolfWay == 0){
+            this.wolf.translation = [wx, wz, wy + 0.01];
+            if (wy >= 15){
+                wolfWay = 1;
+            }
+        } else if (wolfWay == 1){
+            this.wolf.translation = [wx, wz, wy - 0.01];
+            if (wy <= -14){
+                wolfWay = 0;
+            }
+        }
+        // Fox moving
+        if (foxWay == 0){
+            this.fox.translation = [fx + 0.01, fz, fy];
+            if (fx >= 16){
+                foxWay = 1;
+            }
+        } else if (foxWay == 1){
+            this.fox.translation = [fx - 0.01, fz, fy];
+            if (fx <= -2.5){
+                foxWay = 0;
+            }
+        }
+        // Horse moving
+        if (horseWay == 0){
+            this.horse.translation = [hx, hz, hy + 0.01];
+            if (hy >= 12){
+                horseWay = 1;
+            }
+        } else if (horseWay == 1){
+            this.horse.translation = [hx, hz, hy - 0.01];
+            if (hy <= -14){
+                horseWay = 0;
+            }
+        }
+        // Cat moving
+        if (catWay == 0){
+            this.cat.translation = [cx + 0.01, cz, cy];
+            if (cx >= 12){
+                catWay = 1;
+            }
+        } else if (catWay == 1){
+            this.cat.translation = [cx - 0.01, cz, cy];
+            if (cx <= -6){
+                catWay = 0;
+            }
+        }
     }
 
     render() {
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera, this.light);
     }
 
     resize(width, height) {
